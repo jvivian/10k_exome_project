637ce71454e7c28ec546d707d66cfc28d7959cd5
John Vivian <jtvivian@gmail.com>
2015-02-28 17:42:08 -0800

    format update

--- Code/GAWD/GAWD_main.py
+++ Code/GAWD/GAWD_main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 # John Vivian
 
-'''
+"""
 Galaxy Automatic Workflow Decomposer
 
 Accepts a Galaxy Workflow (.ga) and tool-descriptors (.xml) to decompose a galaxy's graph structure
@@ -13,56 +13,59 @@ into discrete steps.
 2.  Parse .GA into input_data and tool objects.
 3.
 
-'''
-
+"""
 
 import json
 import xml.etree.ElementTree as ET
 import os
 import fnmatch
 import argparse
+import sys
 
 
-class input_data():
-    '''Input Data Object'''
+class InputData():
+    """Input Data Object"""
+
     def __init__(self, attributes):
-        self.name = attributes['name']          # This should be "Input dataset".
-        self.id = attributes['id']              # Reference number for the input data
-        self.tool_id = attributes['tool_id']    # For input data should be null (Except for Genetorrent)
-        self.inputs = attributes['inputs']      # self.inputs['name'] yields actual name
-        self.input_connections = attributes['input_connections'] # All inputs should have this field be empty
-        self.outputs = attributes['outputs']    # Should be empty
-        self.type = attributes['type']          # Should be: "data_input"
-        self.level = 0                          # Level in the graph structure
+        self.name = attributes['name']  # This should be "Input dataset".
+        self.id = attributes['id']  # Reference number for the input data
+        self.tool_id = attributes['tool_id']  # For input data should be null (Except for Genetorrent)
+        self.inputs = attributes['inputs']  # self.inputs['name'] yields actual name
+        self.input_connections = attributes['input_connections']  # All inputs should have this field be empty
+        self.outputs = attributes['outputs']  # Should be empty
+        self.type = attributes['type']  # Should be: "data_input"
+        self.level = 0  # Level in the graph structure
 
 
-class tool():
-    '''Tool Object'''
+class Tool():
+    """Tool Object"""
+
     def __init__(self, attributes):
-        self.name = attributes['name']          # Informal name; use tool_id
-        self.id = attributes['id']              # Reference number for the tool
-        self.tool_id = attributes['tool_id']    # Specifies tool_id (name)
+        self.name = attributes['name']  # Informal name; use tool_id
+        self.id = attributes['id']  # Reference number for the tool
+        self.tool_id = attributes['tool_id']  # Specifies tool_id (name)
         self.input_connections = attributes['input_connections']
-        self.outputs = attributes['outputs']    # 
-        self.type = attributes['type']          # Should be: "tool"
-        self.postjob = attributes['post_job_actions'] #
-        self.level = None                       # Set to None until defined
+        self.outputs = attributes['outputs']  #
+        self.type = attributes['type']  # Should be: "tool"
+        self.postjob = attributes['post_job_actions']  #
+        self.level = None  # Set to None until defined
 
     def get_input_vals(self):
-        '''Returns a list of input IDs -- This information helps establish precedence in the graph'''
-        return [ x['id'] for x in self.input_connections ]
+        """Returns a list of input IDs -- This information helps establish precedence in the graph"""
+        return [x['id'] for x in self.input_connections]
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-w, --galaxy_workflow', required=True, default=None, help='Specify Galaxy Workflow File (.ga)')
-    parser.add_argument('-d', '--tools_dir', required=True, default=None, help='Specify Galaxy Tools Directory (.xmls)' )
+    parser.add_argument('-d', '--tools_dir', required=True, default=None, help='Specify Galaxy Tools Directory (.xmls)')
     return parser.parse_args()
 
 
 def find_files(pattern, path):
-    '''Find file in path'''
+    """Find file in path"""
+
     result = []
     for root, dirs, files in os.walk(path):
         for name in files:
@@ -71,11 +74,11 @@ def find_files(pattern, path):
     return result
 
 
-def parse_gal_workflow( gal_wflow ):
-    '''Parses Galaxy Workflow'''
-    print gal_wflow
-    json_data=open( gal_wflow[0] )
-    parsed = json.load( json_data )
+def parse_gal_workflow(gal_wflow):
+    """Parses Galaxy Workflow"""
+
+    json_data = open(gal_wflow)
+    parsed = json.load(json_data)
 
     steps = parsed['steps']
 
@@ -86,21 +89,21 @@ def parse_gal_workflow( gal_wflow ):
 
     return parsed['steps']
 
+
 def main():
     args = parse_arguments()
 
-    print "\nGalaxy Workflow Dir:  {}".format( args.galaxy_dir )
+    sys.stdout.write('\nGalaxy Workflow file: {}'.format(args.galaxy_workflow))
+    if not args.galaxy_dir.endswith('.ga'):
+        sys.stdout.err('Improper file type selected: File extension must be (.ga)')
+
+    sys.stdout.write("\nGalaxy Workflow Dir:  {}".format(args.tools_dir))
 
     # Fetch Workflow and XML file names
     tool_xmls = find_files('*.xml', args.galaxy_dir)
-    gal_wflow = find_files('*Slim.ga', args.galaxy_dir)  # Change to *.ga when appropriate
 
     # Parse Galaxy Workflow
-    parse_gal_workflow( gal_wflow )
-
-
-
-
+    parse_gal_workflow(args.galaxy_workflow)
 
 
 if __name__ == '__main__':
