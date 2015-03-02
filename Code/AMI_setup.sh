4d71722f1e75056de484d0b09fc45e4d935323bf
John Vivian <jtvivian@gmail.com>
2015-03-01 22:22:25 -0800

    AMI setup script

---
+++ Code/AMI_setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# This script will prepare an Amazon AMI with all of the tools and data necessary to run the pipeline.
+
+#############
+#   Tools   #
+#############
+
+FILE="/home/profile/.profile" # Check?
+
+sudo apt-get update -y
+sudo apt-get install -y samtools
+sudo apt-get install -y picard-tools
+sudo apt-get install -y wget
+sudo apt-get install -y java
+sudo apt-get install -y git
+sudo apt-get install -y python-pip
+sudo pip install -y PyVCF
+
+# Contest_arrayfree
+wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/Queue-1.4-437-g6b8a9e1-svn-35362.jar
+wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/ContaminationPipeline.scala
+
+# MuTect
+wget https://github.com/broadinstitute/mutect/releases/download/1.1.5/muTect-1.1.5-bin.zip
+unzip muTect-1.1.5-bin.zip
+
+# Maven/Gatk_from_git is unnecessary if we just host the GATK.jar somewhere.
+# Maven ???
+sudo wget http://apache.mesi.com.ar/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
+tar zxvf apache-maven-3.2.5-bin.tar.gz
+sudo mv apache-maven-3.2.5 /usr/local/bin
+sudo rm -f apache-maven-3.2.5-bin.tar.gz
+# Add envs & PATH
+echo >> $FILE
+echo # Maven Attributes
+echo export M2_HOME="/usr/local/bin/apache-maven-3.2.5" >> $FILE
+echo export M2=$M2_HOME/bin >> $FILE
+echo export PATH=$PATH:$M2 >> $FILE
+source $FILE
+
+# GATK-JAR (we can just host this?)
+sudo git clone https://github.com/broadgsa/gatk-protected/
+cd gatk-protected/
+sudo mvn package
+sudo cp target/GenomeAnalysisTK.jar /home/ubuntu/
+cd /home/ubuntu
+
+
+############
+#   Data   #
+############
+
+sudo git clone https://github.com/ucscCancer/pcawg_tools
+sudo mv pcawg_tools/data /home/ubuntu
+sudo rm -f pcawg_tools
+sudo ./data/download.sh
+
+
