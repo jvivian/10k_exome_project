9400f1eb0fbcb60de1f6244573f5fce4f8c20de3
John Vivian <jtvivian@gmail.com>
2015-03-04 14:29:20 -0800

    Moved all data/tools to S3 to improve setup speed

--- Code/AMI_setup.sh
+++ Code/AMI_setup.sh
@@ -19,12 +19,13 @@ sleep 3
 
 sudo apt-get update -y
 sudo apt-get install -y samtools wget openjdk-7-jre git python-pip
-sudo pip install PyVCF
+pip install PyVCF
 
 
 # Make data and tool dirs
 mkdir /home/ubuntu/data
 mkdir /home/ubuntu/tools
+mkdir /home/ubuntu/intermediates
 
 echo
 echo Obtaining .jars and tools from S3
@@ -41,7 +42,6 @@ wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/Queue-1.4-437-g6b8a9e1-sv
 wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/ContaminationPipeline.scala
 # MuTect
 wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/muTect-1.1.5.jar
-cd /home/ubuntu
 
 
 ############
@@ -52,61 +52,32 @@ echo Downloading Precursor Data
 echo
 sleep 3
 
-### From: https://github.com/ucscCancer/pcawg_tools/blob/master/data/download.sh
-#genomic data
 cd /home/ubuntu/data
-wget -r ftp://ftp.sanger.ac.uk/pub/project/PanCancer/
-mv ftp.sanger.ac.uk/pub/project/PanCancer/* ./
-
-# Remove unnecessary folder
-rm -rf ftp.sanger.ac.uk/
-
-#data for ContEst
-wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/SNP6.hg19.interval_list
-wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/hg19_population_stratified_af_hapmap_3.3.fixed.vcf
-wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/gaf_20111020+broad_wex_1.1_hg19.bed
-
-#data for MuTect
-wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/b37_cosmic_v54_120711.vcf
-wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/dbsnp_132_b37.leftAligned.vcf.gz
-gunzip dbsnp_132_b37.leftAligned.vcf.gz
-
-wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/hg19/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.gz
-wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/hg19/1000G_phase1.indels.hg19.sites.vcf.gz
-
-# Fix chromosome naming convention
-zcat Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.gz \
-| perl -pe 's/^chr//' > Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf
-
-zcat 1000G_phase1.indels.hg19.sites.vcf.gz \
-| perl -pe 's/^chr//' > 1000G_phase1.indels.hg19.sites.fixed.vcf
-
-### End From: https://github.com/ucscCancer/pcawg_tools/blob/master/data/download.sh
+# Phase1 VCF
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/1000G_phase1.indels.hg19.sites.fixed.vcf
+# Gold VCF
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf
+# SNP Interval_List
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/SNP6.hg19.interval_list
+# Cosmic VCF
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/b37_cosmic_v54_120711.vcf
+# DBSNP VCF
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/dbsnp_132_b37.leftAligned.vcf
+# PopHap VCF
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/hg19_population_stratified_af_hapmap_3.3.fixed.vcf
+# BED file
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/gaf_20111020%2Bbroad_wex_1.1_hg19.bed
+# Genome.dict
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/genome.dict
+# Genome.fa.fai
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/genome.fa.fai
+# Genome.fa
+wget https://s3-us-west-2.amazonaws.com/bd2k-artifacts/genome.fa
 
 
-# Cleanup
-rm genome.fa.gz.64.sa
-rm genome.fa.gz.64.pac
-rm genome.fa.gz.64.bwt
-rm genome.fa.gz.64.ann
-rm genome.fa.gz.64.amb
-rm 1000G_phase1.indels.hg19.sites.vcf.gz
-rm Mills_and_1000G_gold_standard.indels.hg19.sites.vcf.gz
-rm genome.fa.gz.fai
+echo All Tools and Data have been acquired
 
 
-#################################
-#   Prepare Reference Genome    #
-#################################
-echo
-echo Preparing Reference Genome
-echo
-sleep 3
-
-gunzip /home/ubuntu/data/genome.fa.gz
-cd /home/ubuntu/tools
-samtools faidx $DATA/genome.fa
-java -jar CreateSequenceDictionary.jar R=$DATA/genome.fa O=$DATA/genome.dict
 
 
 
