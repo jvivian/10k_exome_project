003ded817319b20b450ce36ec53688f680cc3c4e
John Vivian <jtvivian@gmail.com>
2015-04-02 19:25:33 -0700

    Updated AMI script to support CCTools

--- Code/AMI_setup.sh
+++ Code/AMI_setup.sh
@@ -18,7 +18,7 @@ echo
 sleep 3
 
 sudo apt-get update -y
-sudo apt-get install -y samtools git wget openjdk-7-jre git python-pip
+sudo apt-get install -y samtools git wget openjdk-7-jre git python-pip coop-computing-tools
 sudo pip install PyVCF
 
 
