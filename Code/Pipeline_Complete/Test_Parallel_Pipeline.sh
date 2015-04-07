86d081e4d8445bc31f2576d8d56322f4ee3f7e6a
John Vivian <jtvivian@gmail.com>
2015-04-07 11:31:35 -0700

    Removed stderr output (didn't work)

--- Code/Pipeline_Complete/Test_Parallel_Pipeline.sh
+++ Code/Pipeline_Complete/Test_Parallel_Pipeline.sh
@@ -6,15 +6,18 @@ set -ex
 DATA="/home/ubuntu/data"
 NORMAL="testexome.pair8.normal.bam"
 TUMOUR="testexome.pair8.tumour.bam"
+CORES=4
+N="N8"
+T="T8"
 
 # Copy and paste help:   ${NORMAL}  ${TUMOUR}  ${DATA}   ${DATA}/${NORMAL}  ${DATA}/${TUMOUR}
 
 # Obtain BAMS
 cd /home/ubuntu/data
 echo Obtaining Tumour/Normal BAMs >> /home/ubuntu/time.txt
-{ time wget https://s3-us-west-2.amazonaws.com/bd2k-test-data/$NORMAL \
+time wget https://s3-us-west-2.amazonaws.com/bd2k-test-data/$NORMAL \
 & wget https://s3-us-west-2.amazonaws.com/bd2k-test-data/$TUMOUR \
-& wait; } 2> /home/ubuntu/time.txt
+& wait
 cd /home/ubuntu/tools
 
 
@@ -25,9 +28,9 @@ INDEXING BAMS
 
 "
 echo Indexing BAMs >> /home/ubuntu/time.txt
-{ time samtools index $DATA/$NORMAL \
+time samtools index $DATA/$NORMAL \
 & samtools index $DATA/$TUMOUR \
-& wait; } 2>> /home/ubuntu/time.txt
+& wait
 
 
 # RealignerTargetCreator ================================================================
@@ -38,19 +41,19 @@ REALIGNER TARGET CREATOR
 "
 # NORMAL
 echo RTC-Normal >> /home/ubuntu/time.txt
-{ time java -Xmx15g -jar GenomeAnalysisTK.jar \
+time java -Xmx15g -jar GenomeAnalysisTK.jar \
 -T RealignerTargetCreator \
--nt 4 \
+-nt $CORES \
 -R ${DATA}/Homo_sapiens_assembly19.fasta \
 -I ${DATA}/${NORMAL} \
 -known ${DATA}/1000G_phase1.indels.hg19.sites.fixed.vcf \
 -known ${DATA}/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf \
 --downsampling_type NONE \
--o ${DATA}/output.normal.intervals; } 2>> /home/ubuntu/time.txt
+-o ${DATA}/output.normal.intervals
 
 # TUMOUR
 echo RTC-Tumour >> /home/ubuntu/time.txt
-{ time java -Xmx15g -jar GenomeAnalysisTK.jar \
+time java -Xmx15g -jar GenomeAnalysisTK.jar \
 -T RealignerTargetCreator \
 -nt 4 \
 -R ${DATA}/Homo_sapiens_assembly19.fasta \
@@ -58,7 +61,7 @@ echo RTC-Tumour >> /home/ubuntu/time.txt
 -known ${DATA}/1000G_phase1.indels.hg19.sites.fixed.vcf \
 -known ${DATA}/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf \
 --downsampling_type NONE \
--o ${DATA}/output.tumour.intervals ; } 2>> /home/ubuntu/time.txt
+-o ${DATA}/output.tumour.intervals
 
 
 # IndelRealigner =======================================================================
@@ -68,8 +71,7 @@ INDEL REALIGNER
 
 "
 # NORMAL
-echo IR-parallel >> /home/ubuntu/time.txt
-{ time java -Xmx15g -jar GenomeAnalysisTK.jar \
+time java -Xmx15g -jar GenomeAnalysisTK.jar \
 -T IndelRealigner \
 -R $DATA/Homo_sapiens_assembly19.fasta \
 -I $DATA/$NORMAL  \
@@ -89,7 +91,7 @@ echo IR-parallel >> /home/ubuntu/time.txt
 -known $DATA/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf \
 -maxReads 720000 -maxInMemory 5400000 \
 -o $DATA/tumour.indel.bam \
-& wait ; } 2>> /home/ubuntu/time.txt
+& wait
 
 # File Cleanup to keep size down
 rm $DATA/$TUMOUR
@@ -105,23 +107,23 @@ Base Recalibrator
 "
 echo BR-Normal >> /home/ubuntu/time.txt
 # NORMAL
-{ time java -Xmx15g -jar GenomeAnalysisTK.jar \
+time java -Xmx15g -jar GenomeAnalysisTK.jar \
 -T BaseRecalibrator \
 -nct 4 \
 -R $DATA/Homo_sapiens_assembly19.fasta \
 -I $DATA/normal.indel.bam \
 -knownSites $DATA/dbsnp_132_b37.leftAligned.vcf \
--o $DATA/normal.recal_data.table ; } 2>> /home/ubuntu/time.txt
+-o $DATA/normal.recal_data.table
 
 echo BR-Tumour >> /home/ubuntu/time.txt
 # TUMOUR
-{ time java -Xmx15g -jar GenomeAnalysisTK.jar \
+time java -Xmx7g -jar GenomeAnalysisTK.jar \
 -T BaseRecalibrator \
 -nct 4 \
 -R $DATA/Homo_sapiens_assembly19.fasta \
 -I $DATA/tumour.indel.bam \
 -knownSites $DATA/dbsnp_132_b37.leftAligned.vcf \
--o $DATA/tumour.recal_data.table ; } 2>> /home/ubuntu/time.txt
+-o $DATA/tumour.recal_data.table
 
 
 # PrintReads ==========================================================================
@@ -132,25 +134,25 @@ Print Reads
 "
 echo PR-Normal >> /home/ubuntu/time.txt
 # NORMAL
-{ time java -Xmx15g -jar GenomeAnalysisTK.jar \
+time java -Xmx7g -jar GenomeAnalysisTK.jar \
 -T PrintReads \
 -nct 4  \
 -R $DATA/Homo_sapiens_assembly19.fasta \
 --emit_original_quals  \
 -I $DATA/normal.indel.bam \
 -BQSR $DATA/normal.recal_data.table \
--o $DATA/normal.bqsr.bam ; } 2>> /home/ubuntu/time.txt
+-o $DATA/normal.bqsr.bam
 
 echo PR-Tumour >> /home/ubuntu/time.txt
 # TUMOUR
-{ time java -Xmx15g -jar GenomeAnalysisTK.jar \
+time java -Xmx7g -jar GenomeAnalysisTK.jar \
 -T PrintReads \
 -nct 4  \
 -R $DATA/Homo_sapiens_assembly19.fasta \
 --emit_original_quals  \
 -I $DATA/tumour.indel.bam \
 -BQSR $DATA/tumour.recal_data.table \
--o $DATA/tumour.bqsr.bam ; } 2>> /home/ubuntu/time.txt
+-o $DATA/tumour.bqsr.bam
 
 # File Cleanup to keep size down
 rm $DATA/normal.indel.bam
@@ -166,7 +168,7 @@ Contest_ArrayFree
 
 "
 echo CAF >> /home/ubuntu/time.txt
-{ time java -Djava.io.tmpdir=~/tmp -Xmx2g \
+time java -Djava.io.tmpdir=~/tmp -Xmx2g \
 -jar Queue-1.4-437-g6b8a9e1-svn-35362.jar \
 -S ContaminationPipeline.scala \
 --reference $DATA/Homo_sapiens_assembly19.fasta \
@@ -175,7 +177,7 @@ echo CAF >> /home/ubuntu/time.txt
 -nbam $DATA/normal.bqsr.bam \
 --popfile $DATA/hg19_population_stratified_af_hapmap_3.3.fixed.vcf \
 --arrayinterval $DATA/SNP6.hg19.interval_list \
---interval $DATA/gaf_20111020+broad_wex_1.1_hg19.bed -run -memory 2 ; } 2>> /home/ubuntu/time.txt
+--interval $DATA/gaf_20111020+broad_wex_1.1_hg19.bed -run -memory 2
 
 
 echo STORING CONTAM VALUE
@@ -196,15 +198,16 @@ MuTect
 
 "
 echo mutect >> /home/ubuntu/time.txt
-{ time java -Xmx15g -jar mutect-1.1.7.jar \
+time java -Xmx15g -jar mutect-1.1.7.jar \
 --analysis_type MuTect \
 --reference_sequence $DATA/Homo_sapiens_assembly19.fasta \
 --cosmic  $DATA/b37_cosmic_v54_120711.vcf \
+--intervals $DATA/whole_exome_agilent_1.1_refseq_plus_3_boosters.targetIntervals.bed \
 --dbsnp $DATA/dbsnp_132_b37.leftAligned.vcf \
 --input_file:normal $DATA/normal.bqsr.bam \
 --input_file:tumor $DATA/tumour.bqsr.bam \
 --out $DATA/MuTect.out \
 --coverage_file $DATA/MuTect.coverage \
---vcf $DATA/MuTect.pair8.vcf ; } 2>> /home/ubuntu/time.txt
+--vcf $DATA/cover.test.vcf
 
 
