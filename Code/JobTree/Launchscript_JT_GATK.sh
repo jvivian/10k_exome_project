9d3bd6583a47ca0277e8b30cfcbee6d494c78316
John Vivian <jtvivian@gmail.com>
2015-04-14 14:07:22 -0700

    Fixed launch script URLs for BAMs

--- Code/JobTree/Launchscript_JT_GATK.sh
+++ Code/JobTree/Launchscript_JT_GATK.sh
@@ -2,8 +2,8 @@
 
 python JT_GATK.py \
 --reference_genome "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.fasta" \
---normal "https://s3-us-west-2.amazonaws.com/bd2k-test-data/testexome.pair9.normal.bam" \
---tumor "https://s3-us-west-2.amazonaws.com/bd2k-test-data/testexome.pair9.tumor.bam" \
+--normal "https://s3-us-west-2.amazonaws.com/bd2k-test-data/pair9.normal.bam" \
+--tumor "https://s3-us-west-2.amazonaws.com/bd2k-test-data/pair9.tumor.bam" \
 --phase "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/1000G_phase1.indels.hg19.sites.fixed.vcf" \
 --mills "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf" \
 --dbsnp "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/dbsnp_132_b37.leftAligned.vcf" \
