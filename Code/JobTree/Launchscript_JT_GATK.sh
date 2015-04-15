eb4da0d9c32e809849be042ed633d99a2325658e
John Vivian <jtvivian@gmail.com>
2015-04-15 14:37:59 -0700

    Added GATK jar req and fixed python name

--- Code/JobTree/Launchscript_JT_GATK.sh
+++ Code/JobTree/Launchscript_JT_GATK.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-python JT_GATK.py \
---reference_genome "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.fasta" \
+python jobTree_GATK_Pipeline.py \
+--reference "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Homo_sapiens_assembly19.fasta" \
 --normal "https://s3-us-west-2.amazonaws.com/bd2k-test-data/pair9.normal.bam" \
 --tumor "https://s3-us-west-2.amazonaws.com/bd2k-test-data/pair9.tumor.bam" \
 --phase "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/1000G_phase1.indels.hg19.sites.fixed.vcf" \
 --mills "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/Mills_and_1000G_gold_standard.indels.hg19.sites.fixed.vcf" \
 --dbsnp "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/dbsnp_132_b37.leftAligned.vcf" \
---cosmic "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/b37_cosmic_v54_120711.vcf"
+--cosmic "https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/b37_cosmic_v54_120711.vcf" \
+--gatk 'https://s3-us-west-2.amazonaws.com/bd2k-artifacts/10k-exomes/GenomeAnalysisTK.jar'
