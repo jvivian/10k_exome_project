b73a664fa1277633c33d20ac55e9ce0c2b8d00d2
John Vivian <jtvivian@gmail.com>
2015-04-17 12:05:04 -0700

    Updated to pass object around -- Works

--- Code/JobTree/jobTree_HelloWorld.py
+++ Code/JobTree/jobTree_HelloWorld.py
@@ -9,22 +9,22 @@ from jobTree.scriptTree.target import Target
 from jobTree.scriptTree.stack import Stack
 from optparse import OptionParser
 
-'''
+
 class HelloWorld(object):
-    def __init__(self, s1):
+    def __init__(self, s1, s2):
         self.s1 = s1
-'''
+        self.s2 = s2
 
-def hello_world(target, s):
+def hello_world(target, hw):
     with open ('hello_world.txt', 'w') as file:
-        file.write(s)
+        file.write(hw.s1)
 
-    target.addChildTargetFn(hello_world_child)
+    target.addChildTargetFn(hello_world_child, (hw,))
 
 
-def hello_world_child(target):
+def hello_world_child(target, hw):
     with open ('hello_world_child.txt', 'w') as file:
-        file.write('Sorry, the cake is a lie.')
+        file.write(hw.s2)
 
 
 if __name__ == '__main__':
@@ -34,12 +34,8 @@ if __name__ == '__main__':
     Stack.addJobTreeOptions(parser)
     options, args = parser.parse_args()
 
-    s1 = 'Test'
-    #hw = HelloWorld(s1)
+    s1 = 'This is a Triumph'
+    s2 = "I'm making a note here; huge success"
+    hw = HelloWorld(s1, s2)
     # Setup the job stack and launch jobTree job
-    #i = Stack(Target.makeTargetFn(hello_world, (hw))).startJobTree(options)
-
-    # Complains that I am giving hello_world 5 arguments instead of 2....
-    i = Stack(Target.makeTargetFn(hello_world, (s1))).startJobTree(options)
-
-    #i = Stack(Target.makeTargetFn(hello_world)).startJobTree(options) # WORKS!
+    i = Stack(Target.makeTargetFn(hello_world, (hw,))).startJobTree(options)
