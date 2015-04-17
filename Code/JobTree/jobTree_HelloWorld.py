c47d21a632fcf373aefa97f6b1236645d671078b
John Vivian <jtvivian@gmail.com>
2015-04-17 11:06:29 -0700

    Spacing.  Accidentally deleted entire script.

--- Code/JobTree/jobTree_HelloWorld.py
+++ Code/JobTree/jobTree_HelloWorld.py
@@ -16,9 +16,11 @@ def hello_world(target):
 
     target.addChildTargetFn(hello_world_child)
 
+
 def hello_world_child(target):
     with open ('hello_world_child.txt', 'w') as file:
         file.write('Sorry, the cake is a lie.')
+
 
 if __name__ == '__main__':
 
@@ -29,3 +31,4 @@ if __name__ == '__main__':
 
     # Setup the job stack and launch jobTree job
     i = Stack(Target.makeTargetFn(hello_world)).startJobTree(options)
+
