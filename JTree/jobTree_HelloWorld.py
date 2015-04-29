e2f03182af24fc6dfb74ef3c44b387bc13dc515e
John Vivian <jtvivian@gmail.com>
2015-04-29 11:45:15 -0700

    fileStore HelloWorld

--- JTree/jobTree_HelloWorld.py
+++ JTree/jobTree_HelloWorld.py
@@ -12,16 +12,20 @@ from optparse import OptionParser
 
 class HelloWorld(object):
     def __init__(self):
-        self.foo_bam = Target.getEmptyFileStoreID()
-        self.bar_bam = Target.getEmptyFileStoreID()
+        self.foo_bam = None
+        self.bar_bam = None
 
 
 def hello_world(target, hw):
 
+    # Create empty FileStoreID for foo_bam
+    hw.foo_bam = target.getEmptyFileStoreID()
+
     with open('foo_bam.txt', 'w') as handle:
-        handle.write('This is a triumph...')
+        handle.write('\nThis should be overwritten\n')
+
     # Update FileStoreID with an associated file
-    target.updateGlobalFile(hw.foo_bam, 'hello_world.txt')
+    target.updateGlobalFile(hw.foo_bam, 'foo_bam.txt')
 
     # Spawn child
     target.addChildTargetFn(hello_world_child, (hw,))
@@ -29,14 +33,19 @@ def hello_world(target, hw):
 
 def hello_world_child(target, hw):
 
-    # Return path for file we'd like to append
     path = target.readGlobalFile(hw.foo_bam)
 
-    with open(path, 'a') as handle:
-        handle.write("\nI'm making a note here: FileStoreID works!")
+    with open(path, 'w') as handle:
+        handle.write("\nFileStoreID works!\n")
 
-    with open('bar_bam.txt') as handle:
-        handle.write("Poor bar_bam didn't want to be left out...")
+    # Create empty FileStoreID for bar_bam
+    hw.bar_bam = target.getEmptyFileStoreID()
+
+    with open('bar_bam.txt', 'w') as handle:
+        handle.write("\nbar_bam made it this far\n")
+
+    # Update FileStoreID
+    target.updateGlobalFile(hw.bar_bam, 'bar_bam.txt')
 
 
 if __name__ == '__main__':
@@ -48,8 +57,6 @@ if __name__ == '__main__':
 
     # Create object that contains our FileStoreIDs
     hw = HelloWorld()
+
     # Setup the job stack and launch jobTree job
     i = Stack(Target.makeTargetFn(hello_world, (hw,))).startJobTree(options)
-
-    if i != 0:
-        raise RuntimeError("Some of the jobs failed")
