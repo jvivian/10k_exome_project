9ee58d0fe1d6e04b0b27f8b2ced571dc5e819a19
John Vivian <jtvivian@gmail.com>
2015-05-05 13:59:42 -0700

    added exception for __setstate__

--- JTree/jobtree_gatk_pipeline.py
+++ JTree/jobtree_gatk_pipeline.py
@@ -93,9 +93,11 @@ class HannesDict(dict):
     >>> d.bam
     12
     """
-
     def __getattr__(self, name):
-        return self[name]
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError()
 
     def __setattr__(self, name, value):
         self[name] = value
